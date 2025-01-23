4bcbae90e9a34b42e6274b87984b4c38f9c16147
Leonardo Moutinho <leonardo.getulio@braip.com>
2025-01-23 16:44:44 -0300

    added: queue: updated mock

--- queue/interface_mock.go
+++ queue/interface_mock.go
@@ -59,7 +59,7 @@ func (mr *MockQueueIMockRecorder) ConsumeStream(arg0, arg1, arg2 interface{}) *g
 }
 
 // Produce mocks base method.
-func (m *MockQueueI) Produce(arg0 context.Context, arg1 string, arg2 Message) error {
+func (m *MockQueueI) Produce(arg0 context.Context, arg1 string, arg2 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Produce", arg0, arg1, arg2)
 	ret0, _ := ret[0].(error)
